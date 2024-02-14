e3d47c35f6b44b605b449c43b92fff39cde18685
Trevor John <trevor@john.tj>
2024-02-14 12:26:51 -0500

    Initial commit

---
+++ lib/omniauth/quickbooks.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "quickbooks/version"
+require_relative "omniauth/strategies/quickbooks"
+
+module Omniauth
+  module Quickbooks
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
