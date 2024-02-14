d503232ac1b9416669a79a323aa12eef67d26827
Trevor John <trevor@john.tj>
2024-02-14 11:53:06 -0500

    Initial commit

---
+++ lib/quickbooks/oauth2.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "oauth2/version"
+
+module Quickbooks
+  module Oauth2
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
